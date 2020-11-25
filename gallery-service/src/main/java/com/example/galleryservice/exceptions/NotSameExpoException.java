762c99a86a02df1a04bd402f25e32e5227037acb
MrSandman5 <mrsandman138@gmail.com>
2020-11-25 12:15:46 +0300

    update gitignore

--- gallery-service/src/main/java/com/example/galleryservice/exceptions/NotSameExpoException.java
+++
@@ -1,9 +0,0 @@
-package com.example.galleryservice.exceptions;
-
-import javax.validation.constraints.NotNull;
-
-public class NotSameExpoException extends Exception {
-    public NotSameExpoException(@NotNull final String message) {
-        super(message);
-    }
-}
