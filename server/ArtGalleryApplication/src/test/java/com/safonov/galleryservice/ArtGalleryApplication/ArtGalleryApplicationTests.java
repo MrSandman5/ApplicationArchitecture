6d11cc61720e2feca5d134fefd66bc7fb32eeed0
MrSandman5 <mrsandman138@gmail.com>
2020-11-25 12:09:40 +0300

    project init

---
+++ server/ArtGalleryApplication/src/test/java/com/safonov/galleryservice/ArtGalleryApplication/ArtGalleryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.safonov.galleryservice.ArtGalleryApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtGalleryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
